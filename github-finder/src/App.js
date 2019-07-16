d5073791b0869bb15639ac1054519120869b9338
sergeincode <starynets.sergey@gmail.com>
2019-07-16 18:41:43 +0300

    second commit

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
 import './App.css';
 
+import axios from 'axios';
+
 class App extends Component {
+
+  state = {
+    users: [],
+    loading: false
+  };
+
+  async componentDidMount() {
+    this.setState( {loading: true} );
+
+    const res = await axios.get(`https://api.github.com/users?client_id=
+      ${process.env.REACT_APP_GITHUB_CLIENT_ID}
+      &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+    this.setState({ users: res.data, loading: false });
+  }
+
   render() {
     return (
       <div className='App'>
         <Navbar />
+        <div className="container">
+          <Users users={this.state.users} loading={this.state.loading} />
+        </div>
       </div>
     );
   }
