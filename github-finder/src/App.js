9df73fd641ca445b0ec4a7fb8b0d7078ceb08aee
sergeincode <starynets.sergey@gmail.com>
2019-07-18 22:37:18 +0300

    add routing, User and Repos pages

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -1,8 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Alert from './components/layout/Alert';
 import Users from './components/users/Users';
+import User from './components/users/User';
 import Search from './components/users/Search';
+import About from './components/pages/About';
 import './App.css';
 
 import axios from 'axios';
@@ -10,19 +13,11 @@ import axios from 'axios';
 class App extends Component {
   state = {
     users: [],
+    user: {},
+    repos: [],
     loading: false,
-    alert: null
+    alert: null,
   };
-
-  // async componentDidMount() {
-  //   this.setState( {loading: true} );
-
-  //   const res = await axios.get(`https://api.github.com/users?client_id=
-  //     ${process.env.REACT_APP_GITHUB_CLIENT_ID}
-  //     &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-
-  //   this.setState({ users: res.data, loading: false });
-  // }
 
   searchUsers = async text => {
     this.setState({ loading: true });
@@ -34,6 +29,26 @@ class App extends Component {
     this.setState({ users: res.data.items, loading: false });
   };
 
+  getUser = async username => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(`https://api.github.com/users/${username}
+    ?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
+      &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+    this.setState({ user: res.data, loading: false });
+  }
+
+  getUserRepos = async username => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc
+    &client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
+      &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+    this.setState({ repos: res.data, loading: false });
+  }
+
   clearUsers = () => {
     this.setState({
       users: [],
@@ -44,28 +59,45 @@ class App extends Component {
   setAlert = (msg, type) => {
     this.setState({
       alert: { msg, type }
-    })
-    setTimeout(() => this.setState({ alert: null}), 5000)
+    });
+    setTimeout(() => this.setState({ alert: null }), 5000);
   };
 
   render() {
-    const { users, loading } = this.state;
+    const { users, user, repos, loading } = this.state;
 
     return (
-      <div className="App">
-        <Navbar />
-        <div className="container">
-          <Alert alert={this.state.alert} />          
-          <Search
-            searchUsers={this.searchUsers}
-            clearUsers={this.clearUsers}
-            showClear={users.length > 0 ? true : false}
-            setAlert={this.setAlert}
-          />
-         
-          <Users users={users} loading={loading} />
+      <Router>
+        <div className="App">
+          <Navbar />
+          <div className="container">
+            <Alert alert={this.state.alert} />
+            <Switch>
+              <Route
+                exact
+                path="/"
+                render={props => (
+                  <Fragment>
+                    <Search
+                      searchUsers={this.searchUsers}
+                      clearUsers={this.clearUsers}
+                      showClear={users.length > 0 ? true : false}
+                      setAlert={this.setAlert}
+                    />
+                    <Users users={users} loading={loading} />
+                  </Fragment>
+                )}
+              />
+              <Route exact path="/about" component={About} />
+              <Route exact path="/user/:login" render={props => (
+                <User { ...props } getUser={this.getUser} getUserRepos={this.getUserRepos} repos={repos} user={user} loading={loading} />
+              )} />
+            </Switch>
+
+            
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
